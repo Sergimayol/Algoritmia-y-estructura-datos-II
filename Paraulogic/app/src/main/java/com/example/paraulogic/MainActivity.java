403b5d2d9d396df5b28a86e0066cdd28321249de
Sergi Mayol <sergi2808@gmail.com>
2022-03-31 12:18:05 +0200

    updated

--- Paraulogic/app/src/main/java/com/example/paraulogic/MainActivity.java
+++ Paraulogic/app/src/main/java/com/example/paraulogic/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.paraulogic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private int[] listaIDbotones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.listaIDbotones = new int[7];
+        this.listaIDbotones[0] = R.id.letraObligatoria;
+        this.listaIDbotones[1] = R.id.letra1;
+        this.listaIDbotones[2] = R.id.letra2;
+        this.listaIDbotones[3] = R.id.letra3;
+        this.listaIDbotones[4] = R.id.letra4;
+        this.listaIDbotones[5] = R.id.letra5;
+        this.listaIDbotones[6] = R.id.letra6;
+        for (int i = 0; i < this.listaIDbotones.length; i++) {
+            Button button = (Button) findViewById(this.listaIDbotones[i]);
+            button.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        Button button = (Button) findViewById(id);
+        changeTextViewText(button.getText().toString(), id);
+    }
+
+    private void changeTextViewText(String s, int i) {
+        TextView textView = (TextView) findViewById(i);
+        textView.setText(s);
     }
 }
